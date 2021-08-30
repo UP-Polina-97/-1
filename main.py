4468ffe2c2c483645ef0431f4e21374e16acc974
Polina <ustinovapolina1@gmail.com>
2021-08-30 17:39:38 +0200

    added and fixed soe stuff

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ from time import sleep
 
 ## this is function to get photo data from vk
 class VKphoto_send_to_yadisk():
-owner_id = input("please give your id: ")
-token_for_ya = input("please write your token for yandex: ")
-    url = 'https://api.vk.com/method/photos.getAll'
-    token_for_vk =
-    data = []
-    data1 = []
+    def __init__(self):
+        self.token = token
+        self.data = []
+        self.data1 = []
+        #self.token_for_yandex = input("please write your token for yandex: ")
 
-    def get_photos_vk_data(offset=0, count=50):
+
+    def get_photos_vk_data(self, offset=0, count=50, ):
+        #data = []
+        #data1 = []
+        url_vk = 'https://api.vk.com/method/photos.getAll'
         params = {
             'access_token': token_for_vk,
-            'user_id': owner_id,
+            'user_id': user_id,
             'extended': 1,
             'offset': offset,
             'count': count,
@@ -29,15 +32,19 @@ token_for_ya = input("please write your token for yandex: ")
             'skip_hidden': 0,
             'v': 5.131
         }
-        res = requests.get(url, params=params).json()
+        res = requests.get(url_vk, params=params).json()
         rrr = res['response']['items']
         for k in rrr:
-            data.append(k['likes'])
-            data1.append(k['sizes'][-1])
+            self.data.append(k['likes'])
+            self.data1.append(k['sizes'][-1])
 
-    def __init__(self, token: str):
-        self.token = token
+        #print(self.data)
+        #print(self.data1)
 
+    #def save_photo_from_vk(self):
+
+
+#yandex disk send there
     def upload(self, file_path: str):
 
        url = "https://cloud-api.yandex.net:443/"
@@ -55,27 +62,29 @@ token_for_ya = input("please write your token for yandex: ")
        pprint(req.json())
        pprint(req)
        uploader_url = req.json()['href']
-       with open(file_path, 'rb') as file:
-            files = requests.put(uploader_url, data=file)
+       #requests.post(url + url_extra, headers=headers, params={'path': 'py44/img.jpeg', 'url': })
+       for url in self.data1:
+           requests.post(url + url_extra, headers=headers, params={'path': 'py44/img.jpeg', 'url': url['url']})
 
-       print(files)
 
 if __name__ == '__main__':
+# Запрашиваем у пользователя данные для работы  in vk
+    #tmp_folder = ...
+    #user_id = str(input("please give your id: "))
+    user_id = 552934290
+    token_for_vk = "34f4f11807efbe8c1b82a0850fa70f137c556b7221acde76b2acbf4e7d90fc485f5bd4fdd4ccb496e8e95"
+    #user_id = str(input("please give your id: "))
+
+# information for the yandex disk, in order to send it to your disk
     #path_to_file =
-    #loop for different file
-    for k in data1:
-        path_to_file = (k['url'])
-    token = token_for_ya
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
-
-## progress bar function 1
+    path_to_file = "photo.jpeg"
+    token = "AQAAAABXPW06AAdM1-YiAlNVtE3xk5EvzmZuI7U"
+    #token_for_yandex = input("please write your token for yandex: ")
+    #uploader = VKphoto_send_to_yadisk(token)
+    #result = uploader.upload(path_to_file)
 
 
-#def loadbar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='>'):
-#	percent = ('{0:.' + str(decimals) + 'f}').format(100 * (iteration/float(total)))
-#	filledLength = int(length * iteration // total)
-#	bar = fill * filledLength + '-' * (length - filledLength)
-#	print(f'\r{prefix} |{bar}| {percent}% {suffix}', end='\r')
-#	if iteration == total:
-#		print()
+pprint(VKphoto_send_to_yadisk().get_photos_vk_data())
+pprint(VKphoto_send_to_yadisk())
+
+
